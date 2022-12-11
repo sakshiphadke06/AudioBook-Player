6fca978442ad8fe86242b546ceefdc3934084728
sakshiphadke06 <sakshi.v.phadke@gmail.com>
2022-12-11 20:45:53 +0530

    Signed-off-by: sakshiphadke06 <sakshi.v.phadke@gmail.com>

---
+++ strings.py
@@ -0,0 +1,8 @@
+print ("This is String Concatenation")
+fname = "darshan " #first variable
+lname = "ganeshpure" #second variable
+fullname = fname + lname #concatenation of two variables
+print(fullname)
+print (fname * 5) #to print the string multiple times
+
+# print("fname + 3") this will give error since string and number cannot be concatinated
